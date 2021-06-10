9c354de712da6a9e4b4172ff5f7bdebcdb3af4e5
Asim <asim005011997@gmail.com>
2021-06-10 11:35:47 +0530

    useRef

--- src/Para.js
+++ src/Para.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function Para(props) {
     const [update, setUpdate] = useState(false);
-    return (
+    const ref = useRef(null);
+
+    useEffect(()=>{
+        ref.current.focus();
+    }, [update])
+ 
+        return (
         <div>
             <a onClick={()=>setUpdate(true)} style={{cursor:'pointer', marginTop:'10rem'}}>Click me</a>
             {update?(<p> I am the paragraph.</p>):''}
+            <input  type="text" ref={ref}/>
         </div>
     );
 }
